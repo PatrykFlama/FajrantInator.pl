efc0981fad0d4fbb808a3fe0599dd0666e44ded6
PatrykFlama <patrykflama@interia.pl>
2024-01-12 19:10:21 +0100

    fix: database uri option

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose
     .connect(`mongodb://` + 
-             (process.env.DB_USER && process.env.DB_PASS ? `${process.env.DB_USER}:${process.env.DB_PASS}@` : ``) + 
-             `${process.env.DB_HOST}:${process.env.DB_PORT}/db_FajrantInator`)
+             (process.env.DB_URI ? `${process.env.DB_URI}` : 
+                (process.env.DB_USER && process.env.DB_PASS ? `${process.env.DB_USER}:${process.env.DB_PASS}@` : ``) + 
+                `${process.env.DB_HOST}:${process.env.DB_PORT}`) +
+             `/db_FajrantInator`)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error(err));
