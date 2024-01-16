15b99a265bc51db0d02482a971526b573b79ffcd
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2024-01-16 16:33:58 +0100

    feature: default admin account

--- src/database/index.js
+++ src/database/index.js
@@ -8,3 +8,23 @@ mongoose
              `/db_FajrantInator`)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error(err));
+
+// if there is no 'admin' account create one with default password
+
+async function addAdmin() {
+    const User = require('./schemas/Users');
+    const { hashPassword } = require('../utils/helpers');
+
+    const adminExists = await User.findOne({ type: 'admin' });
+    if (!adminExists) {
+        const newAdmin = new User({
+            username: 'admin',
+            password: hashPassword('admin'),
+            type: 'admin',
+            email: 'admin@admin.com',
+        });
+        await newAdmin.save();
+    }
+}
+
+addAdmin();
