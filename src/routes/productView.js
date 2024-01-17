a0f4841f8e9563d976f7dac33dbd3738c0aaa636
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2024-01-17 08:35:30 +0100

    fix: typo

--- src/routes/productView.js
+++ src/routes/productView.js
@@ -108,7 +108,7 @@ router.post('/rateProduct', async (req,res)=>{
     }
 });
 
-rotuer.post('/deleteProduct', async (req,res)=>{
+router.post('/deleteProduct', async (req,res)=>{
     if(req.session.account.type !== 'admin'){
         res.status(403).send('Forbidden');
         return;
@@ -130,7 +130,7 @@ router.post('/deleteRating', async (req,res)=>{
         res.status(403).send('Forbidden');
         return;
     }
-    
+
     try {
         const { productID, userID } = req.body;
         const product = await Products.findOne({ _id: productID });
